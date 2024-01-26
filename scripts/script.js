843bbca076cf764dfb4cf06969c19c1cf7dee1c8
Даниил Кривошеев <gr414_krdse@lab-407-5.tomtit-tomsk.ru>
2024-01-26 13:17:49 +0700

    Methods Part 2 updateProgress, MoveFirtColm ...

--- scripts/script.js
+++ scripts/script.js
@@ -66,5 +66,73 @@ const app = new Vue({
           this.thirdColumn = this.thirdColumn.filter(group => group.id !== groupId);
           this.saveDataToLocalStorage();
         },
+        updateProgress: function (card, item) {
+            const checkedCount = card.items.filter(item => item.checked).length;
+            const progress = (checkedCount / card.items.length) * 100;
+            card.isComplete = progress === 100;
+      
+            if (this.secondColumn.length === 5) {
+              const areAllSecondColumnComplete = this.secondColumn.every(note => note.isComplete);
+      
+              this.firstColumn.forEach(note => {
+                note.items.forEach(item => {
+                  if (item.checked) {
+                    item.disabled = true;
+                  } else {
+                    item.disabled = areAllSecondColumnComplete;
+                  }
+                });
+              });
+            }
+      
+            if (item.checked) {
+              item.disabled = true;
+            } else {
+              item.disabled = false;
+              this.checkDisableFirstColumn();
+            }
+      
+            this.checkMoveCard();
+          },
+      
+          MoveFirstColm: function () {
+            this.firstColumn.forEach(card => {
+              const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
+      
+              const isMaxSecondColumn = this.secondColumn.length >= 5;
+      
+              if (isMaxSecondColumn && progress >= 50) {
+                this.firstColumn.forEach(note => {
+                  note.items.forEach(item => {
+                    item.disabled = true;
+                  });
+                });
+              }
+      
+              if (progress >= 50 && !isMaxSecondColumn) {
+                this.secondColumn.push(card);
+                this.firstColumn.splice(this.firstColumn.indexOf(card), 1);
+                this.MoveSecondColm();
+              }
+            });
+          },
+      
+          MoveSecondColm: function () {
+            this.secondColumn.forEach(card => {
+              const progress = (card.items.filter(item => item.checked).length / card.items.length) * 100;
+              if (progress === 100) {
+                card.isComplete = true;
+                card.lastChecked = new Date().toLocaleString();
+                this.thirdColumn.push(card);
+                this.secondColumn.splice(this.secondColumn.indexOf(card), 1);
+                this.MoveFirstColm();
+              }
+            })
+          },
+      
+          checkMoveCard: function () {
+            this.MoveFirstColm();
+            this.MoveSecondColm();
+          },
     }
 })
